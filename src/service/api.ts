d69e1b8e204f1f0ed7f8be755ebff39fc39c7ea0
josmar-jr <josmartrigueiro.jr@gmail.com>
2022-12-14 11:48:17 -0300

    fix: change image url prod

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = process.env.API_URL ?? "http://localhost:3000";
+const baseUrl = process.env.API_URL ?? "https://ticket-white-party.vercel.app";
 
 export const api = axios.create({
   baseURL: `${baseUrl}/api`,
